e51771d2da1419a4de8774b394b8e623da675d3a
IronistM <m.parzakonis@statsravingmad.com>
2014-12-04 22:58:54 +0200

    Change read.table to read.xls

--- R Scripts/doc_master_table.R
+++ R Scripts/doc_master_table.R
@@ -1,5 +1,6 @@
 # Now we can merge them in order to run some
-# diagnostics
+# diagnostics. Taken from
+# http://psychwire.wordpress.com/2011/06/03/merge-all-files-in-a-directory-using-r-into-a-single-dataframe/
 setwd("reports/")
 
 # this list the files
@@ -10,12 +11,12 @@ for (file in file_list)
   # if the merged dataset doesn't exist, create it
   if (!exists("dataset"))
   {
-    dataset <- read.table(file, header=TRUE, sep="\t")
+    dataset <- read.xls(file, header=TRUE, sep="\t")
   }
   # if the merged dataset does exist, append to it
   if (exists("dataset"))
   {
-    temp_dataset <-read.table(file, header=TRUE, sep="\t")
+    temp_dataset <-read.xls(file, header=TRUE, sep="\t")
     dataset<-rbind(dataset, temp_dataset)
     rm(temp_dataset)
   }
