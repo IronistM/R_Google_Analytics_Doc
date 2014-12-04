40b4719ccff5c0dd163452ecda335fc870982d6d
IronistM <m.parzakonis@statsravingmad.com>
2014-12-04 22:58:21 +0200

    Wordings
    Rewrite comments in the for() loop

--- R Scripts/writing_to_xls.R
+++ R Scripts/writing_to_xls.R
@@ -1,13 +1,12 @@
 # Start getting the information! ------------------------------------------
 require("dataframes2xls")
 for (i in 1:length(accounts))
-{ 
+{
   tmp_goals <- get_goals(account.id = accounts[i])
   tmp_profiles <- get_profiles(account.id = accounts[i])
   tmp_webproperties <- get_webproperties(account.id = accounts[i])
   tmp_segments <- get_segments()
   tmp_custom_sources <- get_custom_sources()
-  # Let's write the results to files
-  # Attempt 2 : xls files
+  # Let's write the results to xls files
   write.xls(c(tmp_webproperties,tmp_profiles,tmp_goals,tmp_segments,tmp_custom_sources), file=paste0("ga_doc_",websiteUrl[i],".xls"))
 }
